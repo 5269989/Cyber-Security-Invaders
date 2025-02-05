804eea03b921559182fc9d473c57e9b41c1778d6
5269989 <5269989@student.gloscol.ac.uk>
2025-02-05 17:24:03 +0000

    Add files via upload

---
+++ ClearLeaderboard.py
@@ -0,0 +1,27 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+def reset_leaderboard():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    url = 'https://5269989.pythonanywhere.com/delete_all_scores'
+    
+    try:
+        # Send the DELETE request with the Authentication
+        response = requests.delete(url, auth=HTTPBasicAuth(username, password))
+
+        if response.status_code == 200:
+            print("All scores have been deleted successfully!")
+        else:
+            try:
+                error_data = response.json()
+                print(f"Failed to delete scores. Error: {error_data}")
+            except requests.exceptions.JSONDecodeError:
+                # If decoding fails, print error
+                print(f"Failed to delete scores. Reason: {response.text}")
+
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+
+reset_leaderboard()
